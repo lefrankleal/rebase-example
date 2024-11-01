c72d34dd4352ca83792b2135561e187b3c777bfb
franklinn.leal <falg2@hotmail.com>
2024-11-01 18:53:55 -0500

    update readme, update next config, update gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  amp: {
+    NextConfig: {},
+    headers: [
+      {
+        name: "X-Frame-Options",
+        value: "DENY",
+      },
+      {
+        name: "X-Content-Type-Options",
+        value: "nosniff",
+      },
+      {
+        name: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
